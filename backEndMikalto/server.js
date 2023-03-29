a6763cd2913545b16b06dc4553687bc7fc7dcfeb
dutta_ankit <ankit.dutta@itobuz.com>
2023-03-29 13:30:13 +0530

    Version 7

--- backEndMikalto/server.js
+++ backEndMikalto/server.js
@@ -1,18 +1,20 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const port = 3400;
+const port = 3800;
 
 const pathName = path.join(__dirname + "/data.json");
 const data = JSON.parse(fs.readFileSync(pathName, "utf-8"));
 
-http.createServer((req, res) => {
+http
+  .createServer((req, res) => {
     try {
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.end(JSON.stringify(data));
     } catch (err) {
       console.log(err);
     }
-  }).listen(port, () => {
+  })
+  .listen(port, () => {
     console.log(`Listening to port: ${port} `);
   });
--- frontEndMikalto/script.js
+++ frontEndMikalto/script.js
@@ -1,26 +1,30 @@
 let res;
 
 let swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  async function getData() {
-    res = await fetch(`http://localhost:3400`)
-         .then((res) => {
-           return res.json();
-         })
-         .catch((err) => {
-           alert(err);
-         });
-       if (!res.error) {
-         console.log(res);
-       } else {
-         console(res.error.message);
-       }
-       document.getElementById("landingBg").style.backgroundImage = `url(${res[0].image})`;
-       document.getElementById("landingSubText").textContent = `${res[0].landingText}`;
-     }
-     getData();
+async function getData() {
+  res = await fetch(`http://localhost:3800`)
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      alert(err);
+    });
+  if (!res.error) {
+    console.log(res);
+  } else {
+    console(res.error.message);
+  }
+  document.getElementById(
+    "landingBg"
+  ).style.backgroundImage = `url(${res.landingSec.image})`;
+  document.getElementById(
+    "landingSubText"
+  ).textContent = `${res.landingSec.landingText}`;
+}
+getData();
